636952d13ddb74789818c3e155dcc9a77c7c6d23
Etza <topimorita@gmail.com>
2019-06-19 17:41:15 +0100

    Index and About pages finish. Working on Resources page

--- build.sh
+++ build.sh
@@ -13,3 +13,4 @@ mkdir -p _site
 
 ./templates/default "3ml Home" index.html > _site/index.html
 ./templates/default "About 3ml" about.html > _site/about.html
+./templates/default "3ml Resources" resources.html > _site/resources.html
