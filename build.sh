b9478da301b7546a79c494104f7e79dfd9e780c9
Etza <topimorita@gmail.com>
2019-06-21 18:58:11 +0100

    Added guidance page to the template

--- build.sh
+++ build.sh
@@ -14,3 +14,4 @@ mkdir -p _site
 ./templates/default "3ml Home" index.html > _site/index.html
 ./templates/default "About 3ml" about.html > _site/about.html
 ./templates/default "3ml Resources" resources.html > _site/resources.html
+./templates/default "3ml Guidance" resources.html > _site/guidance.html
