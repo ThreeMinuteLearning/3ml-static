dd695a377f51387650f0305b96241ef31b68cf8d
Luke Taylor <tekul.hs@gmail.com>
2019-07-07 16:51:19 +0100

    Add template for help videos
    This removes a lot of repetition in the guidance.html page. The
    video titles and URLs are now listed in the help_videos.txt file.
    The build file runs the template against each entry in this file,
    storing the output in an environment variable. The variable is
    expanded by using the envsubst command on the content file in the
    default template.

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Run automatically on detected changes.
+set -e
+
+# To run automatically on detected changes.
 #
 # Linux:
 # while true; do inotifywait -e modify *.html templates/*; ./build.sh; done
@@ -10,6 +12,11 @@
 # fswatch *.html templates/* | xargs -n1 -I{} ./build.sh
 
 mkdir -p _site
+
+export HELP_VIDEOS="$(while IFS=',' read -r heading vurl; do
+  ./templates/help_video "$heading" $vurl
+done < help_videos.txt)"
+
 
 ./templates/default "3ml Home" index.html > _site/index.html
 ./templates/default "About 3ml" about.html > _site/about.html
