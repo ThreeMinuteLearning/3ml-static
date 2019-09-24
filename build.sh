f62d60fa823ad0759cc1ab001939eac7f0e6be20
Luke Taylor <tekul.hs@gmail.com>
2019-09-24 15:10:03 +0100

    Template the curriculum page
    The entries are now loaded from curriculum.txt which is a CSV file
    containing
    
    - The name for the entry
    - The image URL
    - The alt text for the image
    - The URL to the PDF file

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 # To run automatically on detected changes.
 #
-# Linux:
+# Lin
 # while true; do inotifywait -e modify *.html templates/*; ./build.sh; done
 #
 # OSX:
@@ -20,6 +20,11 @@ done < help_videos.txt)"
 export RESEARCHERS="$(while IFS=',' read -r researcher_name img_url pdf_url blurb; do
   ./templates/researcher "$researcher_name" $img_url $pdf_url "$blurb"
 done < researchers.txt)"
+
+export CURRICULUM="$(while IFS=',' read -r curriculum_name img_url img_alt pdf_url; do
+  ./templates/curriculum "$curriculum_name" $img_url "$img_alt" $pdf_url
+done < curriculum.txt)"
+
 
 ./templates/default "3ml Home" index.html > _site/index.html
 ./templates/default "About 3ml" about.html > _site/about.html
