6acb0059842d0d6937ab4d5e7f7c8bfa4b45dba5
Luke Taylor <tekul@pm.me>
2023-09-11 20:39:40 +0100

    Go back to using vega-embed
    Still getting unsafe-eval CSP errors using vega directly. Not sure
    why.

--- assets/js/3ml-admin.js
+++ assets/js/3ml-admin.js
@@ -1,18 +1,7 @@
 var app = Elm.Main.init({flags: navigator.userAgent});
 
 app.ports.elmToVega.subscribe(function(vlSpec) {
-    var vgSpec = vegaLite.compile(vlSpec, {}).spec;
-    var runtimeSpec = vega.parse(vgSpec, null, { ast: true });
-
     window.requestAnimationFrame(function() {
-        var view = new vega.View(runtimeSpec, {
-          expr:      vega.expressionInterpreter,
-          renderer:  'svg',
-          container: '#vis',
-          hover:     false
-        });
-
-        view.runAsync();
-        // vegaEmbed("#vis", spec, {actions: false, renderer: 'svg'}).catch(console.warn);
+        vegaEmbed("#vis", vlSpec, {actions: false, renderer: 'svg', ast: true }).catch(console.warn);
     });
 });
